418a6ab9af8eaa97e66cd0093adeaaae47843539
Marichka0701 <tanechnyk.marichka07@gmail.com>
2023-08-17 11:11:48 +0300

    smth

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,6 @@
+// import React from 'react';
+// import * as React from 'react';
+// import ReactDOM from 'react-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -17,3 +20,6 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+
